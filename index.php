2b1392a0f0b8d8e74494250420a6c7621432b668
Aaron Beckett <becketta@msu.edu>
2016-04-22 18:01:19 -0400

    Set up project structure. Add composer, less, jquery, grunt, and qunitjs support.

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aaron Beckett
+ * Date: 4/22/2016
+ * Time: 5:52 PM
+ */
---
+++ sketch.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Create A Sketch</title>
+  </head>
+  <body>
+    <div id="canvasDiv"></div>
+    <!--[if IE]>
+        <script src="ie-canvas.js"></script>
+    <![endif]-->
+    <script src="site.con.js"></script>
+    <script>
+    	 drawingApp.init();
+	</script>
+  </body>
+</html>
