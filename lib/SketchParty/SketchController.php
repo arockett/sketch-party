c0850742dbefb248e318a8a344f96dd4465664cd
Aaron Beckett <becketta@msu.edu>
2016-04-30 18:17:55 -0400

    Sketches are now saved to the server as image files and the database as filenames.
    Permissions on the images/sketches/ dir are currently set to 777 to allow php to create the image files... this is not ideal and needs to be addressed.

--- lib/SketchParty/SketchController.php
+++ lib/SketchParty/SketchController.php
@@ -11,22 +11,52 @@ namespace SketchParty;
 
 class SketchController extends Controller {
 
+    const SKETCH_DIR = '../images/sketches/';
+
     public function __construct(Site $site, array $post) {
         parent::__construct($site);
 
+        // Get post data
         $title = $_POST['title'];
         $url = $_POST['sketch'];
-        $url = str_replace('data:image/png;base64,', '', $url);
-        $url = str_replace(' ', '+', $url);
-        $data = base64_decode($url);
-        $sketch = new Sketch(array('title' => $title, 'image' => $data));
 
+        // Decode image url created by the canvas
+        $url = explode(";", $url);
+        $extension = explode('/', $url[0])[1];
+        $data = str_replace('base64,', '', $url[1]);
+        $data = str_replace(' ', '+', $data);
+        $data = base64_decode($data);
+
+        // Generate an image file name and save the image data under this name
         $sketches = new Sketches($site);
+        do {
+            $salt = self::randomSalt();
+            $filename = self::SKETCH_DIR . $salt . '.' . $extension;
+        } while($sketches->exists($filename));
+        file_put_contents($filename, $data);
+
+        // Add the sketch to the database
+        $sketch = new Sketch(array('title' => $title, 'imagefile' => $filename));
         if($sketches->save($sketch) === null) {
             $this->result = json_encode(array('ok' => false, 'message' => "Upload failed"));
             return;
         }
 
         $this->result = json_encode(array('ok' => true));
+    }
+
+    /**
+     * Generate a random string of characters for naming sketch image files
+     * @param $len int Length to generate, default is 16
+     * @returns string Salt string
+     */
+    public static function randomSalt($len = 8) {
+        $chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        $l = strlen($chars) - 1;
+        $str = '';
+        for ($i = 0; $i < $len; ++$i) {
+            $str .= $chars[rand(0, $l)];
+        }
+        return $str;
     }
 }
