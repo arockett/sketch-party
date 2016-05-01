aaf5fc5e4ba4aa3da158181d07f2802e6e7f1166
Aaron Beckett <becketta@msu.edu>
2016-05-01 11:38:53 -0400

    Add quote creation page.

---
+++ quote.php
@@ -0,0 +1,34 @@
+<?php
+require 'lib/site.inc.php';
+$view = new \SketchParty\View($site);
+$view->setTitle("Add A Quote");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php echo $view->head(); ?>
+</head>
+<body>
+<div class="content" id="quote">
+    <?php echo $view->header(); ?>
+
+    <form>
+        <p>
+            <label for="source">Source:</label>
+            <input type="text" id="source" name="source" placeholder="Who said this quote?">
+        </p>
+        <p>
+            <textarea id="quote" name="quote" placeholder="Type the quote"></textarea>
+        </p>
+
+        <p>
+            <input type="submit" id="add" name="add" value="Add">
+            <input type="submit" id="discard" name="discard" value="Discard">
+        </p>
+        <div id="message"></div>
+    </form>
+
+    <?php echo $view->footer(); ?>
+</div>
+</body>
+</html>
